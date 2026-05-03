893fb52879971cd8d188c80c267b1c17b1cdafd9
rafaelabimanyu <rafaelabimanyu333@gmail.com>
2026-05-03 23:55:03 +0700

    feat: update staff list with @tiysapos.com domain and execute seeding

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -11,15 +12,43 @@ class UserSeeder extends Seeder
     {
         User::create([
             'name'     => 'Admin',
-            'email'    => 'admin@kasirabi.com',
-            'password' => 'password',
+            'email'    => 'admin@tiysapos.com',
+            'password' => Hash::make('password'),
             'role'     => 'admin',
         ]);
 
         User::create([
-            'name'     => 'Kasir 01',
-            'email'    => 'kasir@kasirabi.com',
-            'password' => 'password',
+            'name'     => 'Senja',
+            'email'    => 'senja@tiysapos.com',
+            'password' => Hash::make('password'),
+            'role'     => 'kasir',
+        ]);
+
+        User::create([
+            'name'     => 'Muthia',
+            'email'    => 'muthia@tiysapos.com',
+            'password' => Hash::make('password'),
+            'role'     => 'kasir',
+        ]);
+
+        User::create([
+            'name'     => 'Melani',
+            'email'    => 'melani@tiysapos.com',
+            'password' => Hash::make('password'),
+            'role'     => 'kasir',
+        ]);
+
+        User::create([
+            'name'     => 'Dorkas',
+            'email'    => 'dorkas@tiysapos.com',
+            'password' => Hash::make('password'),
+            'role'     => 'kasir',
+        ]);
+
+        User::create([
+            'name'     => 'Araxsa',
+            'email'    => 'araxsa@tiysapos.com',
+            'password' => Hash::make('password'),
             'role'     => 'kasir',
         ]);
     }
